d9a171519ec2864795a488eb77ab2c9fb9e507e8
Chan Bormey Mao <chanbormeymao@Chans-MBP.gateway>
2023-01-26 02:16:03 +1100

    set up project

---
+++ BookStoreG4Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreG4Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
