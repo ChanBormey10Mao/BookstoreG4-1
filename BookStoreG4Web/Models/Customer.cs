e9d7b3d0fb910af1c9b4553177d46289a16ab29d
Chan Bormey Mao <chanbormeymao@Chans-MBP.gateway>
2023-01-27 01:13:24 +1100

    Creating Customer Model

---
+++ BookStoreG4Web/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreG4Web.Models
+{
+    public class Customer
+    {
+        //Telling Id is primary Key
+        [Key]
+
+        public string Cus_Id { get; set; }
+        [Required] //Id is required cause it is primary key
+
+        public string Cus_Name { get; set; }
+        public string Cus_Email { get; set; }
+        public string Cus_Password { get; set; }
+
+        //set createDateTime to be the present time
+        //public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+
+
+    }
+}
